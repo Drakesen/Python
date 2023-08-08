0be231edeabb8560deb015bb55423c13c4025ad6
Max <mtom03@mail.ru>
2023-08-08 23:52:11 +0700

    Add file

--- Python для github.py
+++ Python для github.py
@@ -1,5 +1,19 @@
 print("Hello my friend!")
 
+# Треугольник Паскаля
+N = 7
+P = []
+
+for i in range(N):
+    row = [1] * (i+1)
+    for j in range(i+1):
+        if j != 0 and j != i:
+            row[j] = P[i-1][j-1] + P[i-1][j]
+    P.append(row)
+for r in P:
+    print(r)
+
+# Код для авторизации
 pass_true = "password"
 ps = ""
 while ps != pass_true:
